af86cc862fc3c89d93cd75570fd804e0f35709e4
hunter666hz <hzhunter7@gmail.com>
2024-10-17 13:44:14 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   app/favicon.ico
    	modified:   app/globals.css
    	modified:   app/layout.tsx
    	modified:   app/page.tsx
    	modified:   tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,52 @@
+'use client';
 import Image from "next/image";
-
+import hero from './Assets/iphone-mockup.png'
+import { FormEvent } from "react";
 export default function Home() {
+  const FormAction = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email')
+    const password = formData.get('password')
+    const terms = formData.get('terms')
+    console.log(formData);
+    // Your form submission logic here
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="h-screen w-screen  flex items-center justify-center">
+      <div className="w-4/6 h-4/5 bg-white flex flex-row-reverse  ">
+
+        <div className="flex-grow flex flex-col items-center  text-bblue p-5 gap-y-3">
+          <p className="text-2xl font-bold w-4/5">Login</p>
+          <form onSubmit={FormAction} className="flex flex-col gap-y-3 items-start w-4/5 self-center">
+            <div className="flex flex-col gap-y-2">
+
+        <label htmlFor="" className="text-xl ">Email</label>
+        <input placeholder="johm@example.com" type="email" name="email" id="email" className="p-2 focus:outline-none border-2 border-background rounded-md focus:border-2 focus:border-bblue " />
+            </div>
+        <div className="flex flex-col gap-y-2">
+        <label htmlFor="" className="text-xl ">Password</label>
+              
+        <input placeholder="at least 8 charcters " type="password" name="password" id="password" className="p-2 focus:outline-none border-2 border-background rounded-md focus:border-2 focus:border-bblue " />
+            </div>
+            <div className="flex gap-x-1 justify-center  p-2">
+              <input type="checkbox" name="terms" id="terms" />
+              <p className="text-xs font-bold">      By creating an account, you agree to the <a href="#terms-of-service" className=" underline text-blue-400" target="_blank">Terms of service</a>.
+              </p>
+
+            </div>
+            <button className="w-4/5 bg-bblue p-3 rounded-md text-white self-center">Create an account</button>
+          </form>
+      </div>
+      <div className=" w-1/2 flex flex-col items-center justify-between bg-bblue h-full  text-white" >
+          <div className=" flex-grow  w-full flex flex-col items-center justify-center" >
+            <p className="text-2xl font-bold ">Gradie</p>
+            <p className="">Beautiful Gradients in seconds   </p>
+    </div>
+      <Image className="h-3/4 w-4/5"  src={hero} alt="hero picture"  />
+      </div>
+      </div>
     </div>
   );
 }
